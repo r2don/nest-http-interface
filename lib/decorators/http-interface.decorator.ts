e62c03c4919fcd8557274b55a2202a5a4ad75844
Jake Son <sonjeabin@gmail.com>
2023-04-09 16:12:00 +0900

    fix: apply nest's injectable decorator to http interface
    Co-authored-by: imdudu1 <cd80@kakao.com>

--- lib/decorators/http-interface.decorator.ts
+++ lib/decorators/http-interface.decorator.ts
@@ -1,7 +1,10 @@
+import { applyDecorators, Injectable } from "@nestjs/common";
 import { HTTP_INTERFACE_METADATA } from "./constants";
 
 export function HttpInterface(url = ""): ClassDecorator {
-  return (target) => {
+  const decorator: ClassDecorator = (target) => {
     Reflect.defineMetadata(HTTP_INTERFACE_METADATA, url, target);
   };
+
+  return applyDecorators(Injectable, decorator);
 }
