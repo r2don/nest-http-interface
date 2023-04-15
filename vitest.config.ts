c1c020033c26456f7cad548444165cd38828bc38
Jake Son <sonjeabin@gmail.com>
2023-04-15 13:39:19 +0900

    chore: add test coverage setting
    Co-authored-by: imdudu1 <cd80@kakao.com>

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,5 +3,9 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     setupFiles: "./global.vitest.ts",
+    coverage: {
+      provider: "c8",
+      reporter: "lcov",
+    },
   },
 });
