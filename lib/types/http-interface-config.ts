74c581f5f388c17f7bde112f019b0bb70ff8a163
Jake Son <sonjeabin@gmail.com>
2023-05-13 21:02:30 +0900

    feat: add circuit breaker support

--- lib/types/http-interface-config.ts
+++ lib/types/http-interface-config.ts
@@ -1,8 +1,10 @@
 import { type ClassTransformOptions } from 'class-transformer';
+import type CircuitBreaker from 'opossum';
 import { type HttpClient } from './http-client.interface';
 
 export interface HttpInterfaceConfig {
   timeout?: number;
   httpClient?: HttpClient;
   transformOption?: ClassTransformOptions;
+  circuitBreakerOption?: CircuitBreaker.Options;
 }
